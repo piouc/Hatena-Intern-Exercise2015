dfff06b1f350a67bb23f8007dac3ca59f3f73596
piou <piou@hotmail.co.jp>
2016-01-01 15:23:16 +0900

    JS-3 GUI

--- js/js-3.js
+++ js/js-3.js
@@ -1,1 +1,12 @@
 // 課題 JS-3 の実装をここに記述してください。
+document.addEventListener('DOMContentLoaded', () => {
+	const tableContainer = document.getElementById('table-container')
+	const logInput = document.getElementById('log-input')
+	const submitButton = document.getElementById('submit-button')
+
+	submitButton.addEventListener('click', () => {
+		const source = logInput.value
+		tableContainer.innerHTML = ''
+		createLogTable(tableContainer, parseLTSVLog(source))
+	})
+})
