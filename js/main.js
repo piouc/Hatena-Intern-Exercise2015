eb157d0de988c5dbf2f8e33e797b1fd9d0988a1d
piou <piou@hotmail.co.jp>
2016-01-01 15:20:58 +0900

    課題JS-1 parseLTSVLog

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 // 課題 JS-1: 関数 `parseLTSVLog` を記述してください
 
+function parseLTSVLog(str){
+	return str
+		.split('\n')
+		.filter(line => line !== '') //空行削除
+		.map(line => {
+			return line
+				.split('\t')
+				.map((attr) => {
+					const [key, value] = attr.split(':')
+					return [key, isFinite(value) ? Number(value) : value]
+				})
+				.reduce((obj, keyValue) => {
+					const [key, value] = keyValue
+					return Object.assign({}, obj, {
+						[key]: value
+					})
+				}, {})
+		})
+}
+
 // 課題 JS-2: 関数 `createLogTable` を記述してください
