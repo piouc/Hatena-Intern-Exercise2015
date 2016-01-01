b13bec5d5f91af920fd5a2d10f251a237c806c59
piou <piou@hotmail.co.jp>
2016-01-01 15:22:19 +0900

    課題JS-2 createLogTable

--- js/main.js
+++ js/main.js
@@ -21,3 +21,44 @@ function parseLTSVLog(str){
 }
 
 // 課題 JS-2: 関数 `createLogTable` を記述してください
+
+/**
+ * [createElement description]
+ * @param  {String} name     HTML tag name
+ * @param  {Object} props    attributes
+ * @param  {Array}  children 
+ * @return {HTMLElement}          HTMLElement
+ */
+function createElement(name, props = {}, children = []){
+	const element = document.createElement(name)
+
+	Object.keys(props).forEach(key => element.setAttribute(key, props[key]))
+	children.forEach(child => element.appendChild(child instanceof HTMLElement ? child : document.createTextNode(child.toString())))
+
+	return element
+}
+
+function createLogTable(container, data){
+
+	const labels = data.reduce((labels, record) => {
+		Object.keys(record).forEach(label => {
+			if(labels.indexOf(label) < 0){
+				labels.push(label)
+			}
+		})
+		return labels
+	}, [])
+
+	const table = createElement('table', {}, [
+		createElement('thead', {}, [
+			createElement('tr', {}, labels.map(label => createElement('th', {}, [label])))
+		]),
+		createElement('tbody', {},
+			data.map(record => createElement('tr', {}, 
+				Object.keys(record).map(key => createElement('td', {}, [record[key]])))
+			)
+		)
+	])
+
+	container.appendChild(table)
+}
